e4492c1077b2d6e4c1eb13e68d6a13ece16250b6
Saikowshik007 <58332764+Saikowshik007@users.noreply.github.com>
2020-07-03 19:28:31 +0530

    Update all.js

--- js/all.js
+++ js/all.js
@@ -269,7 +269,8 @@ document.getElementById("loc").value=location;
 });
 }
 function updatedata(){
-	var name,phone,email,age,lastdonated,group,location,docid,availability;
+	var name,phone,email,age,lastdonated,group,location,docid;
+	String availability;
 	availability="1";
 	firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
